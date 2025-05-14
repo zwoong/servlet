d7e880185aecd057e56c5bf41a3d03193e287c1a
zwoong <zwoong6867@gmail.com>
2025-05-14 17:38:33 +0900

    ✨ 유연한 컨트롤러2 - v5

--- survlet/src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ survlet/src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -5,7 +5,11 @@ import hello.servlet.web.frontcontroller.MyView;
 import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import hello.servlet.web.frontcontroller.v5.adaptor.ControllerV3HandlerAdapter;
+import hello.servlet.web.frontcontroller.v5.adaptor.ControllerV4HandlerAdapter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -30,12 +34,18 @@ public class FrontControllerServletV5 extends HttpServlet {
 
   private void initHandlerAdaptors() {
     handlerAdaptors.add(new ControllerV3HandlerAdapter());
+    handlerAdaptors.add(new ControllerV4HandlerAdapter());
   }
 
   private void initHandlerMappingMap() {
     handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
     handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
     handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+    // v4 추가
+    handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+    handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+    handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
   }
 
   @Override
@@ -58,7 +68,6 @@ public class FrontControllerServletV5 extends HttpServlet {
   }
 
   private MyHandlerAdapter getHandlerAdaptor(Object handler) {
-    MyHandlerAdapter a;
     for (MyHandlerAdapter adapter : handlerAdaptors) {
       if (adapter.supports(handler)) {
         return adapter;
