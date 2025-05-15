6596591c7728903f2e9da6b776f1354525e084cf
zwoong <zwoong6867@gmail.com>
2025-05-15 21:43:54 +0900

    ✨ RedirectAttribute

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Controller
@@ -80,10 +81,18 @@ public class BasicItemController {
 //    return "basic/item";
 //  }
 
+//  @PostMapping("/add")
+//  public String addItemV5(Item item) {
+//    itemRepository.save(item);
+//    return "redirect:/basic/items/" + item.getId();
+//  }
+
   @PostMapping("/add")
-  public String addItemV5(Item item) {
-    itemRepository.save(item);
-    return "redirect:/basic/items/" + item.getId();
+  public String addItemV5(Item item, RedirectAttributes redirectAttributes) {
+    Item savedItem = itemRepository.save(item);
+    redirectAttributes.addAttribute("itemId", savedItem.getId());
+    redirectAttributes.addAttribute("status", true);
+    return "redirect:/basic/items/{itemId}";
   }
 
   @GetMapping("/{itemId}/edit")
